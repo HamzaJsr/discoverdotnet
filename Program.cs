20a739e2a7a2c026a9ea067be13754c8b8b789f7
HamzaJsr <gestiondev.j@gmail.com>
2024-12-04 22:27:25 +0100

    add all files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+
+app.MapGet("/get", () => "Hello GET !");
+app.MapDelete("/delete", () => "Hello DELETE !");
+app.MapPost("/post", () => "Hello POST !");
+app.MapPut("/put", () => "Hello PUT !");
+app.MapPatch("/patch", () => "Hello PATCH !");
+
+app.Run();
